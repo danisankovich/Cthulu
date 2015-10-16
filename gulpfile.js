77748f4c10fb77ce6d7ac87096018e6d9ea26649
Michael Sankovich <misankovich@gmail.com>
2015-10-16 01:59:55 -0700

    gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -10,27 +10,36 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     notify = require('gulp-notify'),
     cache = require('gulp-cache'),
+    watch = require('gulp-watch'),
     livereload = require('gulp-livereload'),
+    browser = require('browser-sync'),
+    run = require('run-sequence'),
     del = require('del');
 
 gulp.task('styles', function() {
   return sass('public/stylesheets/style.scss', { style: 'expanded' })
     .pipe(autoprefixer('last 2 version'))
-    .pipe(gulp.dest('public/stylesheets'))
+    .pipe(gulp.dest('public/stylesheets/css/css'))
     .pipe(rename({suffix: '.min'}))
     .pipe(minifycss())
-    .pipe(gulp.dest('public/stylesheets'))
+    .pipe(gulp.dest('public/stylesheets/css/css'))
     .pipe(notify({ message: 'Styles task complete' }));
 });
 
-gulp.task('scripts', function() {
-  return gulp.src('/public/javascripts/*.js')
-    .pipe(jshint('.jshintrc'))
-    .pipe(jshint.reporter('default'))
-    .pipe(concat('main.js'))
-    .pipe(gulp.dest('/public/javascripts'))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(uglify())
-    .pipe(gulp.dest('/public/javascripts'))
-    .pipe(notify({ message: 'Scripts task complete' }));
+gulp.task('default', function() {
+    gulp.start('styles', 'watch');
+});
+
+gulp.task('clean', function(cb) {
+    del(['public/stylesheets/css/css'], cb);
+});
+
+gulp.task('reload', function(){
+  browser.reload();
+});
+
+gulp.task('watch', function () {
+    return watch('public/stylesheets/style.scss', function() {
+          gulp.start('styles', 'reload');
+        });
 });
