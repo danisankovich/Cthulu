1dd0864fcb448dba1372787177208765ee15b3bc
Michael Sankovich <misankovich@gmail.com>
2016-01-29 13:48:05 -0500

    updated gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,39 @@
-var gulp = require('gulp'),
-    $ = require('gulp-load-plugins')(),
-    sass = require('gulp-ruby-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    minifycss = require('gulp-minify-css'),
-    uglify = require('gulp-uglify'),
-    rename = require('gulp-rename'),
-    notify = require('gulp-notify'),
-    cache = require('gulp-cache'),
-    watch = require('gulp-watch'),
-    browser = require('browser-sync'),
-    run = require('run-sequence'),
-    del = require('del');
+var gulp = require('gulp');
+var del = require('del');
+// var run = require('gulp-run');
+var concat = require("gulp-concat");
+// var addsrc = require('gulp-add-src');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-ruby-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var minifycss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var livereload = require('gulp-livereload');
+var nodemon = require('gulp-nodemon');
+var watch = require('gulp-watch');
+var browserSync = require('browser-sync').create();
 
 gulp.task('styles', function() {
   return sass('public/stylesheets/style.scss', { style: 'expanded' })
     .pipe(autoprefixer('last 2 version'))
-    .pipe(gulp.dest('public/stylesheets/css/css'))
-    .pipe(rename({suffix: '.min'}))
     .pipe(minifycss())
-    .pipe(gulp.dest('public/stylesheets/css/css'))
-    .pipe(notify({ message: 'Styles task complete' }));
-});
-
-gulp.task('default', function() {
-    gulp.start('styles', 'watch');
-});
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('public/dist/'));
+  });
 
 gulp.task('clean', function(cb) {
-    del(['public/stylesheets/css/css'], cb);
+    del(['public/dist'], cb);
 });
 
 gulp.task('reload', function(){
   browser.reload();
+});
+
+gulp.task('js', function() {
+  gulp.src("public/javascripts/script.js")
+  .pipe(concat("bundle.js"))
+  .pipe(uglify({mangle: false}))
+  .pipe(gulp.dest("public/dist/"));
 });
 
 gulp.task('watch', function () {
@@ -39,3 +41,6 @@ gulp.task('watch', function () {
           gulp.start('styles', 'reload');
         });
 });
+gulp.task("nodemon", function() { nodemon(); });
+
+gulp.task('default', ['nodemon', 'js', 'styles', 'watch']);
